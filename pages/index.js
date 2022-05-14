8dc8ea01a265af99ded59de0583725c4bf05cb69
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-13 18:23:58 -0700

    feat: add content to Head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Rumble</title>
+        <meta name="description" content="Tinder for Restaurants" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
     </div>
   )
 }
