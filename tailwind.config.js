434771c6c3a4d2911321561816f4b34278d93753
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-13 18:24:09 -0700

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sunset-orange': '#FF6161',
+        'limed-spruce': '37474F',
+        'dark-jungle-green': '1F2427',
+        'white': 'FFFFFF',
+        'star-dust': '#A59E9E',
+        'star-dust-dark': '#9D9D9D',
+        'reef-gold': '#A48111',
+        'moccasin': '#8D733F',
+        'black': '000000',
+        'military-green': '#65793E',
+        'flat-blue': '#3074AB',
+      },
+    },
   },
   plugins: [],
 }
