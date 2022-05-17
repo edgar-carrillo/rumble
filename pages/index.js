85a7619ca8001c594da385b491151a43e33f0036
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-17 09:40:32 -0700

    feat: add desktop page and login page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
+// Components
+import DesktopPage from './DesktopPage';
+import LoginPage from './LoginPage';
+
 export default function Home() {
   return (
-    <div>
+    <div className="bg-dark-jungle-green">
       <Head>
         <title>Rumble</title>
         <meta name="description" content="Tinder for Restaurants" />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
+      {/* --------------- DesktopPage --------------- */}
+      <div className="sm:block hidden">
+        <DesktopPage />
+      </div>
+      {/* --------------- Main Home --------------- */}
+      <div className="sm:hidden block">
+        <LoginPage />
+      </div>
     </div>
   )
 }
