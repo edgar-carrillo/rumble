47de2d1a9a654071da4746bd6eca647ab935ed97
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-19 20:40:20 -0700

    feat: create Back Button

---
+++ components/BackBtn.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+// Assets
+import leftArrow from '../public/images/left-arrow.svg';
+
+const btnClass = "z-50 absolute bg-transparent h-full w-full cursor-pointer"
+
+const btnContainerClass = "relative w-11 h-11 bg-limed-spruce flex items-center justify-center";
+
+const shadowContainerClass = "inline-block shadow-light rounded-full overflow-hidden"
+
+const iconClass = "w-1/2 h-1/2 relative";
+
+const BackButton = ({ clickHandler }) => {
+  return (
+    <div className={shadowContainerClass}>
+      <div className={btnContainerClass}>
+        <button type="button" className={btnClass} onClick={() => clickHandler()}>
+        </button>
+        <div className={iconClass}>
+            <Image className="absolute" src={leftArrow} alt="" layout="fill"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BackButton;
