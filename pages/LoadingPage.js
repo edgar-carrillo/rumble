4d2e186f4824f017722d8d638c8238a81fae1590
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-19 20:40:35 -0700

    feat: create loading page

---
+++ pages/LoadingPage.js
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="h-screen w-screen bg-dark-jungle-green flex justify-center items-center">
+      <p className="text-sunset-orange font-logo text-5xl">Loading...</p>
+    </div>
+  );
+}
