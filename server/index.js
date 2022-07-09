78d68fad9eb71a4eeb1b9ec35f9aed1a9c5ed635
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-09 16:29:24 -0700

    feat: add express server and user route

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+  const app = express();
+
+  // routes
+  const users = require('./routes/users');
+
+  app.use('/users', users);
+
+  app.all('*', (req, res) => {
+    return handle(req, res)
+  });
+
+  const PORT = process.env.SERVER_PORT || 3000;
+
+  app.listen(PORT, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${PORT}`)
+  });
+});
