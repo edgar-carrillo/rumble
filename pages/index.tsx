0fd4303abab57ec245cc452ee3c07191f3f1b812
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-09 18:39:20 -0700

    refactor: remove Desktop page and only leave loginpage component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 
 // Components
-import DesktopPage from './DesktopPage';
 import LoginPage from './login';
 
 export default function Home() {
@@ -12,14 +11,7 @@ export default function Home() {
         <meta name="description" content="Tinder for Restaurants" />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
-      {/* --------------- DesktopPage --------------- */}
-      <div className="sm:block hidden">
-        <DesktopPage />
-      </div>
-      {/* --------------- Main Home --------------- */}
-      <div className="sm:hidden block">
-        <LoginPage />
-      </div>
+      <LoginPage />
     </div>
   )
 }
