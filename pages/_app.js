a4f14fc2a1404967c92a238402f096c8b5fa6f6d
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-09 18:38:01 -0700

    feat: add responsiveness to ALL pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 
+// Components
+import DesktopPage from './DesktopPage';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <div className="sm:block hidden">
+        <DesktopPage />
+      </div>
+      <div className="sm:hidden block">
+        <Component {...pageProps}  />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
