dd9e0ed7f5a8746669690481a94b432d184b7db5
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-11 12:17:13 -0700

    feat: add login-form with Name and Location Page

---
+++ pages/login-form.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+// Firebase
+import { signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebaseConfig';
+
+// Components
+import LoadingPage from '../components/loading';
+import NamePage from '../components/loginForm/NamePage';
+import LocationPage from '../components/loginForm/LocationPage';
+
+export default function LoginForm() {
+  const [user, loading] = useAuthState(auth);
+  const [currPage, setCurrPage] = useState('name-page');
+  const router = useRouter();
+
+  function logout() {
+    signOut(auth)
+      .then(() => {
+        router.push('/');
+      });
+  }
+
+  return (
+    <div>
+      { loading ?
+        <LoadingPage /> :
+        <form>
+          <NamePage
+            userName={user?.displayName || ''}
+            isVisible={currPage === 'name-page'}
+            goPrevPage={() => logout()}
+            goNextPage={() => setCurrPage('location-page')}
+          />
+          <LocationPage
+            userLocation={''}
+            isVisible={currPage === 'location-page'}
+            goPrevPage={() => setCurrPage('name-page')}
+            goNextPage={() => setCurrPage('cuisine-page')}
+          />
+        </form>
+      }
+    </div>
+  );
+}
