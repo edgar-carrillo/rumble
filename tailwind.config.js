c88f1eeea39e95a60e6af674b24abe8b3d2461da
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-11 12:10:15 -0700

    feat: add light white and amber colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'limed-spruce': '#37474F',
         'dark-jungle-green': '#1F2427',
         'white': '#FFFFFF',
+        'light-white': 'rgba(255, 255, 255, .5)',
         'star-dust': '#A59E9E',
         'star-dust-dark': '#9D9D9D',
         'reef-gold': '#A48111',
@@ -17,6 +18,7 @@ module.exports = {
         'black': '#000000',
         'military-green': '#65793E',
         'flat-blue': '#3074AB',
+        'amber': '#ffbf00',
       },
       fontFamily: {
         'regular': ['Roboto-Regular'],
