251b125dfad8a12661c632e2fc232f925686f16a
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-17 23:03:07 -0700

    feat: add SelectionContainer

---
+++ components/loginForm/selection/SelectionContainer.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+interface ItemProps {
+  readonly text: string;
+  readonly isSelected: Boolean;
+};
+
+function Item({ text, isSelected }: ItemProps) {
+  const defaultClassName = "cursor-pointer border-b border-solid mb-10";
+  let className = defaultClassName + " text-white border-white";
+  if (isSelected) className = defaultClassName + " text-amber border-amber font-bold";
+
+  return <p className={className}>{text}</p>;
+}
+
+interface SelectionContainerProps {
+  readonly items: [string] | never[];
+  readonly selectionHandler: (arg0: number) => void;
+};
+
+export default function SelectionContainer({
+  items, selectionHandler,
+}: SelectionContainerProps) {
+  const [selectedItem, setSelectedItem] = useState(-1);
+
+  useEffect(() => {
+    if (selectedItem !== -1) selectionHandler(selectedItem);
+  }, [selectedItem, selectionHandler]);
+
+  useEffect(() => {
+    if (!items.length) setSelectedItem(-1);
+  }, [items]);
+
+  return (
+    <ul className="max-h-60 overflow-hidden overflow-y-scroll mb-8">
+      {items.map((item: string, index: number) => (
+        <li key={index} onClick={() => setSelectedItem(index)}>
+          <Item text={item} isSelected={index === selectedItem} />
+        </li>
+      ))}
+    </ul>
+  );
+}
