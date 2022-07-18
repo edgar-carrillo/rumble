23b373c22ba7ec036e85daa03eea3e933fe71ffa
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-17 22:58:42 -0700

    feat: add location page related functions

--- scripts/models/loginForm.tsx
+++ scripts/models/loginForm.tsx
@@ -1,4 +1,6 @@
-const loginForm = {
+import axios from "axios";
+
+const loginFormModel = {
 
   nameErrorHandler: (name: string, customError: string) => {
     if (customError.length > 0) return customError;
@@ -7,10 +9,34 @@ const loginForm = {
     return '';
   },
 
+  locationErrorHandler: function (location: string, customError: string) {
+    if (customError.length > 0) return customError;
+    if (location.length <= 5) return 'Please enter more then 5 characters.';
+    if (location.length >= 40) return 'Please enter less then 40 characters.';
+    return '';
+  },
+
+  getLocations: (location: string) => new Promise((resolve, reject) => {
+    axios.get(`https://api.teleport.org/api/cities/?search=${location}`)
+      .then((response) => {
+        let locations = response.data._embedded['city:search-results']
+          .filter((location: any) => {
+            if (location.matching_full_name.includes('United States')) return location;
+          });
+        resolve(locations);
+      })
+      .catch((err) => reject(err));
+  }),
+
   formatInputName: (name: string) => {
     return name.split(' ').join('-').toLowerCase();
+  },
+
+  formatLocationName: (location: string) => {
+    if (location.length > 40) location = location.slice(0, 41).concat('...');
+    return location;
   }
 
 };
 
-export default loginForm;
+export default loginFormModel;
