769e60adaeb5dd8f9051c04a7eac185cefa6b768
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-02 21:01:13 -0700

    feat: update with text and spinner

--- components/loading.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingPage() {
-  return (
-    <div className="h-screen w-screen bg-dark-jungle-green flex justify-center items-center">
-      <p className="text-sunset-orange font-logo text-5xl">Loading...</p>
-    </div>
-  );
-}
---
+++ components/loading.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+// Assets
+import spinnerSrc from '../public/images/spinner.gif';
+
+interface LoadingPageProps {
+  readonly title: string;
+  readonly description?: string;
+  readonly centerText?: Boolean;
+};
+
+export default function LoadingPage({
+  title, description, centerText,
+ }: LoadingPageProps) {
+  let defaultClassName = "flex flex-col min-h-screen w-screen bg-dark-jungle-green px-10 py-20";
+  let className = defaultClassName;
+
+  return (
+    <div className={className}>
+      <div className={centerText ? "text-center" : ""}>
+        <h1 className="font-light text-white text-5xl pt-5 pb-8">{title}</h1>
+        <p className="text-white text-lg pb-2">{description}</p>
+      </div>
+      <div className="relative flex flex-1 my-8 m-auto justify-center items-center">
+        <div>
+          <Image src={spinnerSrc} alt="spinner gif" height={150} width={150} />
+        </div>
+      </div>
+    </div>
+  );
+}
