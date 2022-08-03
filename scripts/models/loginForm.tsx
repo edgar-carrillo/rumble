5db963131e59afc372037243eb5a45a8356b1695
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-02 17:41:48 -0700

    feat: add updateLocation func

--- scripts/models/loginForm.tsx
+++ scripts/models/loginForm.tsx
@@ -12,7 +12,7 @@ const loginFormModel = {
   locationErrorHandler: function (location: string, customError: string) {
     if (customError.length > 0) return customError;
     if (location.length <= 5) return 'Please enter more then 5 characters.';
-    if (location.length >= 40) return 'Please enter less then 40 characters.';
+    if (location.length >= 100) return 'Please enter less then 100 characters.';
     return '';
   },
 
@@ -38,6 +38,8 @@ const loginFormModel = {
   },
 
   updateName: (username: string) => localStorage.setItem('username', username),
+
+  updateLocation: (location: string) => localStorage.setItem('location', location),
 
 };
 
