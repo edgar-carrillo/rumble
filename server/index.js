bfda15ff378502227939ac8f7e7bffa248061f0c
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-04 20:36:35 -0700

    refactor: rearrange code for readability

--- server/index.js
+++ server/index.js
@@ -1,26 +1,23 @@
-const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
+const express = require('express');
 
 nextApp.prepare().then(() => {
   const app = express();
+  const userRoutes = require('./routes/users');
 
-  // routes
-  const users = require('./routes/users');
-
-  app.use('/users', users);
+  // Register Routes
+  app.use('/users', userRoutes);
 
   app.all('*', (req, res) => {
     return handle(req, res)
   });
 
   const PORT = process.env.SERVER_PORT || 3000;
-
-  app.listen(PORT, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${PORT}`)
+  app.listen(PORT, () => {
+    console.log(`Custom server listening on http://localhost:${PORT}`)
   });
 });
