149e8bb9d0bd2235947a91a2552c1a00460902f3
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-06 15:32:57 -0700

    feat: add User schema

---
+++ server/schemas/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.schema;
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  username: String,
+  photo_url: String,
+  restaurant_preferences: {
+    location: String,
+    price: String,
+    cuisine: String,
+  },
+  favorite_restauraunts: [String],
+  liked_restaurants: [String],
+  disliked_restaurants: [String],
+  swipe_groups: [String],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
