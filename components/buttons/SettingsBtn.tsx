45fe88dac88371d2cd8b08f0dce98129ec1bb4c4
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-10 13:35:18 -0700

    feat: add SettingsBtn component

---
+++ components/buttons/SettingsBtn.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+import settingsIcon from '../../public/images/settings-icon.svg';
+
+interface SettingsBtnProps {
+  readonly clickHandler: () => {};
+};
+
+export default function SettingsBtn({ clickHandler }: SettingsBtnProps) {
+  return (
+    <a href="#" className="relative h-8 w-8" onClick={clickHandler} onTouchStart={clickHandler}>
+      <Image src={settingsIcon} alt="#" objectFit="cover" layout="fill" />
+    </a>
+  );
+}
