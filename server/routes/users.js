70716256cc30d412cfe48cdb409279962ff7844d
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-09 23:40:34 -0700

    feat: delete routes for liked/disliked restaurants
    These routes only delete a single specified restaurant
    in either liked or disliked.

--- server/routes/users.js
+++ server/routes/users.js
@@ -77,4 +77,28 @@ router.post('/dislikedRestaurants', (req, res) => {
     .catch((error) => res.status(404).send(error));
 });
 
+router.delete('/dislikedRestaurant', async (req, res) => {
+  const { restaurantId, userEmail } = req.body;
+  await dbConnect();
+
+  try {
+    await User.updateOne({ email: userEmail }, { $pull: { disliked_restaurants: restaurantId } });
+    res.status(200).send(`Removed ${restaurantId} from disliked restaurants successfully.`);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.delete('/likedRestaurant', async (req, res) => {
+  const { restaurantId, userEmail } = req.body;
+  await dbConnect();
+
+  try {
+    await User.updateOne({ email: userEmail }, { $pull: { liked_restaurants: restaurantId } });
+    res.status(200).send(`Removed ${restaurantId} from liked restaurants successfully.`);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
 module.exports = router;
