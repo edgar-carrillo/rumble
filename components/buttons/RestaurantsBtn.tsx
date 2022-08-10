12b771c2518376f6c151f74e7c2d7422719e5222
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-10 13:31:31 -0700

    feat: add RestaurantsBtn component

---
+++ components/buttons/RestaurantsBtn.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+import restaurantsIcon from '../../public/images/restaurants-icon.svg';
+
+interface RestaurantsBtnProps {
+  readonly clickHandler: () => {};
+};
+
+export default function RestaurantsBtn({ clickHandler }: RestaurantsBtnProps) {
+  return (
+    <a href="#" className="relative h-10 w-10" onClick={clickHandler} onTouchStart={clickHandler}>
+      <Image src={restaurantsIcon} alt="#" objectFit="cover" layout="fill" />
+    </a>
+  );
+}
