4fc6e6dce8c1e34a4e725157bfdb0972c999dceb
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-21 13:20:48 -0700

    feat: add inactive mode and small size
    Inactive mode darkens the like button
    and small makes the component a smaller size.

--- components/buttons/LikeBtn.tsx
+++ components/buttons/LikeBtn.tsx
@@ -1,21 +1,30 @@
 import Image from 'next/image';
 
-// Assets
 import heartIcon from '../../public/images/heart-icon.svg';
 
 interface LikeBtnProps {
-  readonly clickHandler: () => {};
+  readonly clickHandler: () => void;
+  readonly small?: boolean;
+  readonly active?: boolean;
 };
 
-export default function LikeBtn({ clickHandler }: LikeBtnProps) {
-
-  let containerClass = 'relative flex w-14 h-14 shadow-lg shadow-black rounded-full justify-center items-center bg-sunset-orange';
-
+export default function LikeBtn({ clickHandler, small, active }: LikeBtnProps) {
+  let containerClass = 'relative flex shadow-lg rounded-full justify-center items-center transition-all duration-150';
   const btnClass = 'absolute h-full w-full cursor-pointer rounded-full';
+  let imgHeightAndWidth = '25px';
+
+  if (small) {
+    containerClass += ' w-10 h-10';
+    imgHeightAndWidth = '20px';
+  }
+  else containerClass += ' w-14 h-14';
+
+  if (active) containerClass += ' bg-sunset-orange shadow-black';
+  else containerClass += ' bg-dark-jungle-green shadow-white';
 
   return (
     <div className={containerClass}>
-      <Image height="25px" width="25px" src={heartIcon} alt="#" />
+      <Image height={imgHeightAndWidth} width={imgHeightAndWidth} src={heartIcon} alt="#" />
       <input type="button" className={btnClass} onClick={clickHandler} onTouchStart={clickHandler} />
     </div>
   );
