2ef763ce9dcc8969f825a87ba53cd6d3776220fd
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-21 13:53:38 -0700

    feat: add yelp image domains
    Allow the yelp image domains when retrieving data from
    yelp.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     config.experiments.topLevelAwait = true;
     return config;
   },
+  images: {
+    domains: [
+      's3-media1.fl.yelpcdn.com',
+      's3-media2.fl.yelpcdn.com',
+      's3-media3.fl.yelpcdn.com',
+      's3-media4.fl.yelpcdn.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
