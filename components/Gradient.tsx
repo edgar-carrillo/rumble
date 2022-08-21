5df63e1d3111323d85be56e1e9f9d711e93ce13a
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-21 13:28:00 -0700

    feat: add pink to Gradient
    The gradient can now appear is pink instead of black.

--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -1,5 +1,13 @@
-export default function Gradient() {
+interface GradientProps {
+  readonly pink?: boolean;
+};
+
+export default function Gradient({ pink }: GradientProps) {
+  let className = 'absolute h-full w-full bg-gradient-to-t';
+  if (pink) className += ' from-sunset-orange';
+  else className += ' from-black';
+
   return (
-    <div className="absolute h-full w-full bg-gradient-to-t from-black"></div>
+    <div className={className}></div>
   );
 }
