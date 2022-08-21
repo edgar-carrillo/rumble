8b835219aaa5e5fa921683e9430e82e53c0e6c38
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-21 13:30:56 -0700

    feat: add ImageWithGradients component

--- components/ImageWithGradients.tsx
+++ components/ImageWithGradients.tsx
@@ -8,19 +8,21 @@ interface ImageWithGradientsProps {
   readonly alt?: string;
   readonly light?: boolean;
   readonly dark?: boolean;
+  readonly pinkGradient?: boolean;
 };
 
 export default function ImageWithGradients({
-  src, alt, light, dark,
+  src, alt, light, dark, pinkGradient,
  }: ImageWithGradientsProps) {
   const gradients = [];
+  if (pinkGradient) gradients.push(<Gradient key={20} pink />);
 
   let numOfGradients = 2;
   if (dark) numOfGradients = 6;
   else if (light) numOfGradients = 1;
 
   for (let i = 0; i < numOfGradients; i++) {
-    gradients.push(<Gradient key={i} />)
+    gradients.push(<Gradient key={i} />);
   }
 
   return (
