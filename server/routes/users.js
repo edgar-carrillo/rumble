740170bdd78bb9e7e62c2d3cbfb2c5c777cb3291
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-21 21:20:41 -0700

    refactor: delete for disliked and liked restaurants
    Use params instead of queries and also change endpoint
    syntax from 'users/dislikedRestaurants' to
    'users/:user_email/restaurants/disliked/:restaurant_id`

--- server/routes/users.js
+++ server/routes/users.js
@@ -115,25 +115,25 @@ router.post('/:user_email/restaurants/disliked/:restaurant_id', async (req, res)
   res.status(200).send(`Restaurant with id: ${restaurant_id} added to disliked restaurants!`);
 });
 
-router.delete('/dislikedRestaurant', async (req, res) => {
-  const { restaurantId, userEmail } = req.body;
+router.delete('/:user_email/restaurants/disliked/:restaurant_id', async (req, res) => {
   await dbConnect();
+  const { user_email, restaurant_id } = req.params;
 
   try {
-    await User.updateOne({ email: userEmail }, { $pull: { disliked_restaurants: restaurantId } });
-    res.status(200).send(`Removed ${restaurantId} from disliked restaurants successfully.`);
+    await User.updateOne({ email: user_email }, { $pull: { disliked_restaurants: restaurant_id } });
+    res.status(200).send(`Removed ${restaurant_id} from disliked restaurants successfully.`);
   } catch (error) {
     res.status(404).send(error);
   }
 });
 
-router.delete('/likedRestaurant', async (req, res) => {
-  const { restaurantId, userEmail } = req.body;
+router.delete('/:user_email/restaurants/liked/:restaurant_id', async (req, res) => {
   await dbConnect();
+  const { restaurant_id, user_email } = req.params;
 
   try {
-    await User.updateOne({ email: userEmail }, { $pull: { liked_restaurants: restaurantId } });
-    res.status(200).send(`Removed ${restaurantId} from liked restaurants successfully.`);
+    await User.updateOne({ email: user_email }, { $pull: { liked_restaurants: restaurant_id } });
+    res.status(200).send(`Removed ${restaurant_id} from liked restaurants successfully.`);
   } catch (error) {
     res.status(404).send(error);
   }
