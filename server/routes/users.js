c9449c9b1b09bc1d70fccf55a79557217c78880d
Edgar Carrillo <ecarrillo046@gmail.com>
2022-08-21 21:22:44 -0700

    refactor: remove get route for swiped restaurants

--- server/routes/users.js
+++ server/routes/users.js
@@ -16,19 +16,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/swipedRestaurants', async (req, res) => {
-  await dbConnect();
-
-  try {
-    const response = await User.aggregate([{
-      $project: { swiped: { $concatArrays: ['$disliked_restaurants', '$liked_restaurants'] }}
-    }]);
-    res.status(200).send(response[0].swiped);
-  } catch(error) {
-    res.status(404).send(error);
-  }
-});
-
 router.get('/:user_email/restaurants/unswiped/:cuisine/:location', async (req, res) => {
   await dbConnect();
 
